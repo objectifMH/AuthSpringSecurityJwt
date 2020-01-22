d1703ee883b9d7565727c1c2fb0fd5c89b0fb976
unknown <objectifmh@hotmail.com>
2020-01-22 09:52:42 +0000

    ajout d'un admin et d'un utilisateur'

--- src/main/java/fr/panda/base/security/SecurityConfig.java
+++ src/main/java/fr/panda/base/security/SecurityConfig.java
@@ -6,6 +6,8 @@
 package fr.panda.base.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,10 +15,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  *
  * @author gtu
  */
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN", "USER")
+                .and()
+                .withUser("user").password("user").roles("USER");
+    }
     
-    
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+        
+        
+    }
+
 }
