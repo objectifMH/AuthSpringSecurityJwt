5c75a589f3f67ade681a1284ab70738659ce5e4e
unknown <objectifmh@hotmail.com>
2020-01-25 10:57:56 +0000

    spring security configure, on se connecte avec les utilisateurs de la base de d + mdp encoder

--- src/main/java/fr/panda/base/security/SecurityConfig.java
+++ src/main/java/fr/panda/base/security/SecurityConfig.java
@@ -5,11 +5,16 @@
  */
 package fr.panda.base.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -19,18 +24,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}admin").roles("ADMIN", "USER")
-                .and()
-                .withUser("user").password("{noop}user").roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("{noop}admin").roles("ADMIN", "USER")
+//                .and()
+//                .withUser("user").password("{noop}user").roles("USER");
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        //http.formLogin();
-        //http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/messages/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
 
     }
 
